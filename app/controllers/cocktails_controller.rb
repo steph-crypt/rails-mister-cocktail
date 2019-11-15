2015e756f6471dee3135c355992e583a4796d0e3
Sofia Stephenson <sgandsteph@gmail.com>
2019-11-15 17:56:34 +0100

    Add Article scaffold

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -7,14 +7,22 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
   end
 
-#   def new
-#     @cocktail = Cocktail.find(params[:id])
-#     @cocktail = Cocktail.new
-#   end
+  def new
+    @cocktail = Cocktail.new
+  end
 
-#   def create
-#     @cocktail = Cocktail.new(params[:id])
-#     @cocktail.save
-#     redirect_to cocktails_path
-#   end
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show]
-  resources :doses, only: [:create]
-  resources :ingredients, only: [:create]
+  resources :doses, only: :destroy
+  resources :cocktails, only: [ :index, :show, :new, :create ] do
+    resources :doses, only: [ :create, :new ]
+  end
+end
+
+Rails.application.routes.draw do
+  root to: 'articles#index'
+  resources :articles, except: :index
 end

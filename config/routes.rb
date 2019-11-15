6b898b35ab7819b6555be5f9217477e4c26f5094
Sofia Stephenson <sgandsteph@gmail.com>
2019-11-15 18:54:56 +0100

    removed articles routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doses, only: :destroy
   resources :cocktails, only: [ :index, :show, :new, :create ] do
     resources :doses, only: [ :create, :new ]
   end
-end
-
-Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles, except: :index
 end
